8c2900210fe23eb88c5e5128241789d525b8dc56
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2026-03-22 21:16:42 +0530

    TODOs

--- app/webhook/event_handlers/issue_comment_event_handler.py
+++ app/webhook/event_handlers/issue_comment_event_handler.py
@@ -15,9 +15,9 @@ class IssueCommentEventHandler(object):
         comment = payload.get("comment", {}).get("body")
 
         if "@ReviewPilot re-review" in comment:
-            print(f"Re-review comment detected... Re-reviewing the PR...\n")
+            print(f"Re-review comment detected... Re-reviewing the PR...\n") # TODO
         elif "@ReviewPilot review" in comment:
-            print(f"Review comment detected... Reviewing the PR...\n")
+            print(f"Review comment detected... Reviewing the PR...\n") # TODO
         elif "@ReviewPilot summarize" in comment:
-            print(f"Summarize comment detected... Summarizing the PR...\n")
+            print(f"Summarize comment detected... Summarizing the PR...\n") # TODO
 
