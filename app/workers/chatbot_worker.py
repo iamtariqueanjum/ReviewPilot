dad6334aff2a3f93539d3dcaf8e4475bbd1c6909
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2026-05-04 00:31:18 +0530

    lint fixes

--- app/workers/chatbot_worker.py
+++ app/workers/chatbot_worker.py
@@ -1,11 +1,13 @@
+from app.core.logger import logger
+
 from app.core.celery_app import celery_app
 from app.core.services.chatbot_service import ChatbotService
 
 @celery_app.task(bind=True, autoretry_for=(Exception,), retry_backoff=5, retry_kwargs={"max_retries": 3},
                  name="app.workers.chatbot_worker.process_chat_message")
 def process_chat_message(self, installation_id, owner, repo, pr_number, sender, query):
-    print(f"[WORKER] Answering user query") # TODO replace with logger
+    logger.info("[WORKER] START - Answering user query %s %s", sender, query)
     chatbot_service = ChatbotService(owner, repo, pr_number, installation_id=installation_id)
     response = chatbot_service.process_query(sender, query)
-    print(f"[WORKER] Answered user query") # TODO replace with logger
+    logger.info("[WORKER] END - Answered user query %s %s: %s", sender, query, response)
     return response
